3d799bc48a9b83fa1f734eb966aa83852485090d
Lucie <luciebois@netcourrier.com>
2026-06-17 15:22:16 +0200

    feature: Expand the list of basic assets for db seeding

--- src/portfolio_management/core/constants.py
+++ src/portfolio_management/core/constants.py
@@ -16,7 +16,7 @@ BASE_ASSETS: list[AssetModel] = [
         oil=0.0,
     ),
     AssetModel(
-        name=" Amundi Core EURO STOXX 50 UCITS ETF EUR Acc",
+        name="Amundi Core EURO STOXX 50 UCITS ETF EUR Acc",
         isin="LU1681047236",
         esg=False,
         defense=0.0,
@@ -28,5 +28,33 @@ BASE_ASSETS: list[AssetModel] = [
         esg=True,
         defense=0.0,
         oil=0.0,
+    ),
+    AssetModel(
+        name="Amundi CAC 40 UCITS ETF Acc",
+        isin="FR0013380607",
+        esg=False,
+        defense=0.05,
+        oil=0.1,
+    ),
+    AssetModel(
+        name="iShares Core DAX UCITS ETF EUR (Acc)",
+        isin="DE0005933931",
+        esg=False,
+        defense=0.0,
+        oil=0.0,
+    ),
+    AssetModel(
+        name="iShares Core FTSE 100 UCITS ETF (dist)",
+        isin="IE0005042456",
+        esg=False,
+        defense=0.015,
+        oil=0.13,
+    ),
+    AssetModel(
+        name="Amundi IBEX 35 UCITS ETF - Acc",
+        isin="FR0010655746",
+        esg=False,
+        defense=0.0,
+        oil=0.08,
     ),
 ]
